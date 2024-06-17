d22b0b66632a767e31bba16fc58415fd23787b68
mr.root <1mr.rooter3@gmail.com>
2024-06-17 04:14:41 +0330

    custom video player and share , name ... in single page

--- src/components/Games/SinglePage.jsx
+++ src/components/Games/SinglePage.jsx
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
 import VideoPlayer from "./VideoPlayer";
+import { LiaTelegram } from "react-icons/lia";
+import { BsTwitterX } from "react-icons/bs";
+import { FaWhatsapp, FaFacebookF, FaInstagram } from "react-icons/fa";
+import { IoMdLink } from "react-icons/io";
+
 function SinglePage() {
   const videoJsOptions = {
     autoplay: false,
@@ -10,19 +16,95 @@ function SinglePage() {
       {
         src: "/video/trailer/warzoneTrailer.mp4",
         type: "video/mp4",
+        label: "360p",
+        res: 360,
+      },
+      {
+        src: "/video/trailer/warzoneTrailer.mp4",
+        type: "video/mp4",
+        label: "720p",
+        res: 720,
+      },
+      {
+        src: "/video/trailer/warzoneTrailer.mp4",
+        type: "video/mp4",
+        label: "1080p",
+        res: 1080,
       },
     ],
     playbackRates: [0.5, 1, 1.5, 2.5],
   };
 
   return (
-    <div className="w-full  flex">
-      <div className="w-[50rem] h-[30rem]">
-        <VideoPlayer options={videoJsOptions} />
+    <div className="w-full   px-5 mt-6">
+      <div className="flex flex-row p-5  bg-gray-700 rounded-lg">
+        <div className=" w-1/2 h-72">
+          <VideoPlayer options={videoJsOptions} />
+        </div>
+        <AboutGame />
       </div>
-      <div>vjs-icon-placeholders</div>
+      <div className="w-1/2">
+        <AboutPage />
+        <MoreAboutGame />
+      </div>
     </div>
   );
 }
 
 export default SinglePage;
+
+function AboutPage() {
+  return (
+    <>
+      <div className="flex flex-col gap-7">
+        <div className="bg-gray-700 text-white p-5 rounded-lg flex flex-col gap-6 max-h-full  ">
+          <div className="flex gap-2 items-center">
+            <img
+              className="rounded-full border-4 border-gray-600 shadow-lg object-cover w-16 h-16"
+              src="/images/icon/header/Grand-Theft-Auto-San-Andreas.jpg"
+              alt=""
+            />
+            <div>
+              <Link to="">
+                <h3 className="text-lg font-PeydaMed">تیم ایران گیم نت</h3>
+              </Link>
+              <p className="text-sm text-gray-300 pt-1">بازی های IGN</p>
+            </div>
+          </div>
+          <div className=" h-[1px] w-full bg-gray-600" />
+          <div>
+            <h3 className="text-xs text-gray-200 font-PeydaMed">
+              اشتراک گذاری در شبکه های اجتماعی
+            </h3>
+            <div className="flex gap-3 pt-4 px-1">
+              <FaWhatsapp className="w-10 h-5 cursor-pointer transition-all hover:scale-110 hover:text-green-400" />
+              <FaInstagram className="w-10 h-5 cursor-pointer transition-all hover:scale-110 hover:text-red-400" />
+              <LiaTelegram className="w-10 h-5 cursor-pointer transition-all hover:scale-110 hover:text-blue-300" />
+              <FaFacebookF className="w-10 h-5 cursor-pointer transition-all hover:scale-110 hover:text-blue-500" />
+              <BsTwitterX className="w-10 h-5 cursor-pointer transition-all hover:scale-110 hover:text-black" />
+            </div>
+          </div>
+          <div>
+            <h3 className="text-xs text-gray-200 font-PeydaMed">
+              کپی لینک بازی
+            </h3>
+            <div className="flex text-gray-400 gap-3 items-center w-full mt-3 rounded-md bg-gray-400/10 p-2 cursor-text select-all">
+              <IoMdLink />
+              <p className="text-xs">
+                http://localhost:5173/games/page/63456789
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-700 text-white p-5 rounded-lg flex flex-col gap-6 max-h-full ">
+          <h3 className="text-lg font-PeydaMed">اطلاعات سازنده ی بازی </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illum
+            itaque eligendi ad vero qui nemo in sed debitis vel voluptate,
+            nostrum ab aut, ipsam architecto vitae veritatis velit animi?
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Games/VideoPlayer.jsx
+++ src/components/Games/VideoPlayer.jsx
@@ -1,60 +1,19 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
-import "video.js/dist/lang/fa.json"; // بارگذاری فایل زبان فارسی
-import "./VideoPlayer.css"; // ایمپورت فایل CSS سفارشی
-// const VideoPlayer = ({ options }) => {
-//   const videoRef = useRef(null);
-//   const playerRef = useRef(null);
-
-//   useEffect(() => {
-//     // initialize video.js player
-//     if (playerRef.current) {
-//       playerRef.current.dispose();
-//     }
-
-//     const videoElement = videoRef.current;
-//     if (!videoElement) return;
-
-//     playerRef.current = videojs(videoElement, options, () => {
-//       console.log("player is ready");
-//       console.log("Player language:", playerRef.current.language());
-//     });
-
-//     // سفارشی‌سازی کنترل‌ها بعد از بارگذاری پلیر
-//     const controlBar = document.querySelector(".vjs-control-bar");
-//     if (controlBar) {
-//       controlBar.style.backgroundImage = "linear-gradient(to right, #3b35b2, #733cb3)";
-//       controlBar.style.backgroundClip = "text";
-//       controlBar.style.color = "transparent";
-//     }
-
-//     return () => {
-//       if (playerRef.current) {
-//         playerRef.current.dispose();
-//         playerRef.current = null;
-//       }
-//     };
-//   }, [options]);
-
-//   return (
-//     <div data-vjs-player>
-//       <video
-//         ref={videoRef}
-//         className="video-js vjs-default-skin rounded-lg border-2 border-gray-300"
-//       />
-//     </div>
-//   );
-// };
-
-// export default VideoPlayer;
+import "video.js/dist/lang/fa.json";
+import "./VideoPlayer.css";
+import "videojs-hotkeys";
 
 const VideoPlayer = ({ options }) => {
   const videoRef = useRef(null);
   const playerRef = useRef(null);
-
+  document.addEventListener("contextmenu", function (event) {
+    if (event.target.tagName === "VIDEO") {
+      event.preventDefault();
+    }
+  });
   useEffect(() => {
-    // initialize video.js player
     if (playerRef.current) {
       playerRef.current.dispose();
     }
@@ -62,9 +21,23 @@ const VideoPlayer = ({ options }) => {
     const videoElement = videoRef.current;
     if (!videoElement) return;
 
+    // Initialize video.js player
     playerRef.current = videojs(videoElement, options, () => {
-      console.log("player is ready");
-      console.log("Player language:", playerRef.current.language());
+      const bigp = document.querySelector(".vjs-big-play-button");
+      if (bigp) {
+        bigp.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="#FFF" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 mx-auto ml-3">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+        </svg>`;
+      }
+    });
+
+    // Initialize hotkeys plugin
+    playerRef.current.hotkeys({
+      enableModifiersForNumbers: true,
+      seekStep: 10,
+      volumeStep: 0.1,
+      enableVolumeScroll: false,
+      alwaysCaptureHotkeys: true,
     });
 
     return () => {
@@ -76,10 +49,10 @@ const VideoPlayer = ({ options }) => {
   }, [options]);
 
   return (
-    <div data-vjs-player className="my-4 mx-auto max-w-screen-md relative">
-      <video
+    <div data-vjs-player className="my-4 mx-auto max-w-screen-md relative flex-1">
+      <video width={500} height={200}
         ref={videoRef}
-        className="video-js vjs-default-skin rounded-lg border-2 border-gray-300"
+        className="video-js vjs-default-skin rounded-3xl overflow-hidden !w-full !h-full object-none"
       />
     </div>
   );
